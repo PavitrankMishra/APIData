3978b600389fc45706f575a3ce5b48aa6b003228
Pavitrank <pavitrank2002@gmail.com>
2023-08-29 18:28:35 +0530

    Got the real time location of ISS

--- index.js
+++ index.js
@@ -1,69 +1,75 @@
-console.log("This website gets data on request");
+console.log("In this website we can fetch data");
 
-console.log("About to fetch a rainbow");
-
-// Making a map and tiles
-const mymap = L.map('issMap').setView([0,0], 1);
-
-// Making a mrker with custom icon
-var issIcon = L.icon({
-    iconUrl: 'iss.png',
-    iconSize: [50,32],
-    iconAnchor: [25,16],
-});
-const marker = L.marker([0,0], { icon:issIcon}).addTo(mymap);
-
-const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-
-const tileUrl = 'https://{s}.title.openstreetmap.org/{z}/{x}/{y}.png'
-const tiles = L.tileLayer(tileUrl, { attribution });
-tiles.addTo(mymap);
-const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
-
-// fetch("rainbow.jpg").then(response => {
-//     console.log(response);
-//     return response.blob();
-// }).then(blob => {
-//     console.log(blob);
-//     document.getElementById("raifnbow").src = URL.createObjectURL(blob);
-// }).catch(error => {
-//     console.log("Error!");
-//     console.log(error);
-// })
-
-/* Getting data as image */
-// First we fetch image by using fetch function it is returned as a response that the response is converted into the blob format the blob format gets converted into url format that is accepted by the image attribute.
+/* Getting the image of rainbow by using the fetch method */
 
 catchRainbow()
   .then((response) => {
-    console.log("Yay");
+    console.log("Yay!");
   })
   .catch((error) => {
-    console.log("error!");
+    console.log("Error!");
     console.log(error);
   });
 
 async function catchRainbow() {
   const response = await fetch("rainbow.jpg");
-  const blob = await response.blob();
-  document.getElementById("rainbow").src = URL.createObjectURL(blob);
+  const data = await response.blob();
+  document.getElementById("rainbow").src = URL.createObjectURL(data);
+  console.log(data);
 }
 
-/* Getting the Combined Land-Surface Air and Sea-surface Water temperature in °C*/
-chartIt();
+// fetch("rainbow.jpg").then(response => {
+//   console.log(response);
+//   return response.blob();
+// }).then(blob => {
+//   console.log(blob);
+//   document.getElementById("raifnbow").src = URL.createObjectURL(blob);
+// }).catch(error => {
+//   console.log("Error!");
+//   console.log(error);
+// })
 
+/* The above functionality is completed here */
+
+/* Tabular data from csv files */
+/* Combined Land-Surface, air and Sea-Water temperature in °C */
+
+async function getData() {
+  const xs = [];
+  const ys = [];
+  const response = await fetch("ZonAnn.Ts+dSST.csv");
+  // console.log(response);
+  const text = await response.text();
+  // console.log(text);
+  const table = text.split("\n").slice(1);
+  table.forEach((row) => {
+    const column = row.split(",");
+    const year = column[0];
+    const temp = column[1];
+    console.log(year, temp);
+    xs.push(year);
+    ys.push(temp);
+    console.log(parseFloat(temp));
+  });
+  return { xs, ys };
+}
+
+chartIt();
 async function chartIt() {
   const data = await getData();
   const ctx = document.getElementById("chart");
-  const myChart = new Chart(ctx, {
+  const xlabels = data.xs;
+  const ylabels = data.ys;
+
+  new Chart(ctx, {
     type: "line",
     data: {
-      labels: data.xs,
+      labels: xlabels,
       datasets: [
         {
-          label: "Combined Land-Surface Air and Sea-surface Water temperature in °C",
-          data: data.ys,
-          fill:false,
+          label:
+            "Combined Land-Surface Air and Sea-surface Water temperature in °C",
+          data: ylabels,
           borderWidth: 1,
         },
       ],
@@ -78,51 +84,22 @@ async function chartIt() {
   });
 }
 
-getData();
-async function getData() {
-    const xs = [];
-    const ys = [];
-  const response = await fetch("ZonAnn.Ts+dSST.csv");
-  const data = await response.text();
-  console.log(data);
+/* Data is charted on the chart .csv file from above code */
 
-  const table = data.split('\n').slice(1);
-  console.log(table);
-  table.forEach(row => {
-    const columns = row.split(',');
-    const year = columns[0];
-    const temp = columns[1];
-    xs.push(year);
-    ys.push(parseFloat(temp));
-    console.log(year, temp);
-  });
-  return {xs, ys};
-  
-}
 
-/* Getting realtime location of international space station */ 
-
-// const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
-
+/* Getting real time location of ISS */
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
 async function getISS() {
-    const response = await fetch(api_url);
-    const data = await response.json();
-    // console.log(data.latitude);
-    const {latitude, longitude} = data;
-    console.log(latitude);
-    console.log(longitude);
-    // L.marker([latitude, longitude]).addTo(mymap);
-    marker.setLatLng([latitude, longitude]);
-    document.getElementById('lat').textContent = latitude;
-    document.getElementById('lon').textContent = longitude;
+  const response = await fetch(api_url);
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  const {latitude, longitude} = data;
+  console.log(latitude);
+  console.log(longitude);
+  document.getElementById('lat').textContent = latitude;
+  document.getElementById('lon').textContent = longitude;
 }
 
 getISS();
-
-
-
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '© OpenStreetMap'
-// }).addTo(map);
